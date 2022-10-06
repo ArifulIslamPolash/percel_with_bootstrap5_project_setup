3907fe2d6d663a4c3ed136028045ccd6a1fbe550
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-10-06 12:45:41 +0600

    import bootstrap in this project

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import Alert from 'bootstrap/js/dist/alert'
+
+// or, specify which plugins you need:
+import { Tooltip, Toast, Popover } from 'bootstrap'
